63091da1fae6284053d27c14948a1214fa2d671e
Kaue Eduardo <kaue.ke19@gmail.com>
2023-05-04 10:33:53 -0300

    Deleting Cart Items.

--- models/user.js
+++ models/user.js
@@ -25,7 +25,7 @@ const userSchema = new Schema({
   }
 });
 
-userSchema.methods.addToCart = function(product) {
+userSchema.methods.addToCart = function (product) {
   const cartProductIndex = this.cart.items.findIndex(cp => {
     return cp.productId.toString() === product._id.toString();
   });
@@ -45,6 +45,14 @@ userSchema.methods.addToCart = function(product) {
     items: updatedCartItems
   };
   this.cart = updatedCart;
+  return this.save();
+};
+
+userSchema.methods.removeFromCart = function (productId) {
+  const updatedCartItems = this.cart.items.filter(item => {
+    return item.productId.toString() !== productId.toString();
+  });
+  this.cart.items = updatedCartItems;
   return this.save();
 };
 
